a82b7c8d0936fabc3aa437405c1bb33db51febc5
mlwmlw <520cutecat@gmail.com>
2015-04-26 19:05:07 +0800

    fix singleton

--- example/main.jsx
+++ example/main.jsx
@@ -1,28 +1,33 @@
 var Fluxx = require('../src/fluxx');
 var React = require('react');
-var EventEmitter = require('events').EventEmitter;
-var assign = require('object-assign');
 
-var flux = new Fluxx();
-
-var listStore = (function() {
+Fluxx.store('list', function() {
 	var items = [1,2,3,4,5,6];
-	return assign({}, EventEmitter.prototype, {
+	return {
 		getItems: function() {
 			return items;
 		},
 		onPop: function() {
 			items.pop();
 			this.emit('change');
+		},
+		onPush: function(value) {
+			items.push(value)
+			this.emit('change');
 		}
-	});
-})();
+	};
+});
 var List = React.createClass({
+	mixins: [Fluxx.mixin(React)],
 	getInitialState: function() {
-		return {items: listStore.getItems()};
+		this.list = this.flux().getStore('list')
+		return {items: this.list.getItems()};
 	},
 	componentDidMount: function() {
-		listStore.addListener('change', this._onChange);
+		this.list.addListener('change', this._onChange);
+	},
+	componentDidUnmount: function() {
+		this.list.remvoeListener('change', this._onChange);
 	},
 	render: function() {
 		var lis = this.state.items.map(function(i) {
@@ -34,24 +39,54 @@ var List = React.createClass({
 		this.setState(this.getInitialState());
 	}
 });
-flux.register(function(payload) {
-	  if (payload.actionType === 'pop') {
-			listStore.onPop();
-		}
-});
-var Button = React.createClass({
-	action: function() {
-		flux.dispatch({
+
+var actions = Fluxx.action({
+	pop: function() {
+		this.dispatch({
 			actionType: 'pop'
 		});
 	},
+	push: function(data) {
+		this.dispatch({
+			actionType: 'push',
+			value: data
+		});
+	}
+});
+var Button = React.createClass({
+	mixins: [Fluxx.mixin(React)],
 	render: function() {
-		return <button onClick={this.action}>Hello World</button>
+		return <button onClick={this.props.onClick}>{this.props.children}</button>
 	}
 });
 var App = React.createClass({
+	mixins: [Fluxx.mixin(React)],
+	getInitialState: function() {
+		return {};
+	},
+	pop: function() {
+		this.flux().getAction().pop();
+	},
+	push: function() {
+		var list = this.flux().getStore('list');
+		this.flux().getAction().push(list.getItems().length + 1);
+	},
 	render: function() {
-		return <div><Button /><List /></div>
+		return <div>
+				<Button onClick={this.pop}>POP</Button>
+				<Button onClick={this.push}>PUSH</Button>
+				<List />
+			</div>
 	}
 });
-React.render(<App />, document.getElementById('container'));
+var flux = new Fluxx();
+flux.register(function(payload) {
+	
+	if (payload.actionType === 'pop') {
+		flux.getStore('list').onPop();
+	}
+	else if (payload.actionType === 'push') {
+		flux.getStore('list').onPush(payload.value);
+	}
+});
+React.render(<App flux={{flux}} />, document.getElementById('container'));
--- src/fluxx.js
+++ src/fluxx.js
@@ -1,3 +1,83 @@
 var Dispatcher = require('flux').Dispatcher;
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
 
-module.exports = Dispatcher;
+var _storeFactories = {};
+var _instances = []
+var _id = 0;
+var genId = function() {
+	var id = _id;
+	_id++;
+	return id;
+}
+var Fluxx = function() {
+	var _store = [];
+	var F = function() {};
+	var id = genId();
+	F.prototype = new Dispatcher;
+	F.prototype.getStore = function(name) {
+		return _store[name];
+	}
+	
+	F.prototype.createStore = function(name) {
+		return _store[name] = assign({}, EventEmitter.prototype, _storeFactories[name]());
+	}
+	var instance = _instances[id] = new F();
+	F.prototype.getAction = function(name) {
+		name = name || 'global';
+		var actions = {}
+		for(var i in _actions[name]) { 
+			actions[i] = _actions[name][i].bind(instance);
+		}
+		return actions;
+	}
+	for(var i in _storeFactories) {
+		instance.createStore(i);
+	}
+	return instance;
+};
+
+Fluxx.store = function(name, factory) {
+	_storeFactories[name] = factory;
+	for(var i in _instances) {
+		_instances[i].createStore(name);
+	}
+};
+
+var _actions = {};
+Fluxx.action = function(name, actions) {
+	if(!actions) {
+		actions = name;
+		name = 'global';
+	}
+	if(!_actions[name])
+		_actions[name] = {};
+	for(var i in actions) { 
+		_actions[name][i] = actions[i];
+	}
+};
+
+Fluxx.mixin = function(React) {
+	var getContext = function(com) {
+    return com.props.flux || (com.context && com.context.flux);
+	}
+	return {
+		contextTypes: {
+			flux: React.PropTypes.object
+		},
+		childContextTypes: {
+			flux: React.PropTypes.object
+		},
+		componentWillMount: function() {
+		},
+		getChildContext: function() {
+			return {
+				flux: getContext(this),
+			};
+		},
+		flux: function() {
+			return getContext(this).flux;
+		}
+	}
+}
+module.exports = Fluxx;
